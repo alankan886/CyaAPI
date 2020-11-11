581a7a690b3f4b4a58619f91d2b7c257d0e5770a
Alan Kan <alankan2004@gmail.com>
2020-11-11 09:48:24 -0500

    Init tests

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from app import app
+
+
+@pytest.fixture(scope="module")
+def test_client():
+    client = TestClient(app)
+    yield client
