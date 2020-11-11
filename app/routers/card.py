6c98059b7ceff05afc9f62d217af01aa9005c67f
Alan Kan <alankan2004@gmail.com>
2020-11-11 09:45:57 -0500

    Removed put method

--- app/routers/card.py
+++ app/routers/card.py
@@ -16,11 +16,6 @@ def create_card(card: Card):
     return {'Hello': 'Cards'}
 
 
-@router.put('/{card_id}')
-def update_card(card: Card, card_id: int):
-    return {'Hello': 'Cards'}
-
-
 @router.delete('/{card_id}')
 def delete_card(card_id: int):
     return {'Hello': 'Cards'}
